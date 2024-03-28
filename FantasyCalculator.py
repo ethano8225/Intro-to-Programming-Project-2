11e5b9dbfbfa31421a1c168915660ebaa43d3ace
etan8225 <eoconnor8225@gmail.com>
2024-03-27 23:48:46 -0400

    initial

---
+++ FantasyCalculator.py
@@ -0,0 +1,5 @@
+####### Names and Spire ID ###########
+# 1. First Last ########
+# 2. First Last ########
+
+""" Implement the main program and functions below """
---
+++ Player.py
@@ -0,0 +1,5 @@
+####### Names and Spire ID ###########
+# 1. First Last ########
+# 2. First Last ########
+
+""" Implement the Player class and functions below """

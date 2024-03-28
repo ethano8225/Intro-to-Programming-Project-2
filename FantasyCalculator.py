38d488a37180e40f1450f453c79a0b217e2352c7
etan8225 <eoconnor8225@gmail.com>
2024-03-28 01:48:58 -0400

    start

--- FantasyCalculator.py
+++ FantasyCalculator.py
@@ -1,5 +1,65 @@
 ####### Names and Spire ID ###########
-# 1. First Last ########
-# 2. First Last ########
+# 1. Ethan O'Connor 34445111
+# 2. Ryan Blanchette 34620003
 
 """ Implement the main program and functions below """
+import Player
+
+
+P1 = Player.Player("Erling Haaland",     "EPL",          18, 5,  270)
+P2 = Player.Player("Robert Lewandowski", "La Liga",      12, 6,  414)
+P3 = Player.Player("Harry Kane",         "Bundesliga",   30, 6,  472)
+P4 = Player.Player("Son Heung-Min",      "EPL",          14, 8,  660)
+P5 = Player.Player("Julian Alvarez",     "EPL",          8,  8,  915)
+P6 = Player.Player("Leroy Sane",         "Bundesliga",   8, 11,  903)
+P7 = Player.Player("Mohamed Salah",      "EPL",          15, 9,  686)
+P8 = Player.Player("Jude Bellingham",    "La Liga",      16, 3, 1164)
+P9 = Player.Player("Toni Kroos",         "La Liga",      1,  7, 1908)
+P10= Player.Player("Sehrou Guirassy",   "Bundesliga",    21, 1,  494)
+
+playerList= [P1,P2,P3,P4,P5,P6,P7,P8,P9,P10]
+
+def display_menu():
+    print("1-List all players")
+    print("2-List all players with at least a specific number of goals")
+    print("3-List all players in a specific league")
+    print("4-Find a specific player")
+    print("5-Determine the player with the most goals")
+    print("6-Determine total points for all players and print out the values")
+    print("7-Exit\n")
+
+print("Welcome to your Fantasy Team Database!")
+print("====================\n\n")
+
+while True:
+    display_menu()
+    option=input("Enter Command: ")
+
+    
+    if option =="1":
+        print("Name   \t\t\tLeague\t\tGoals\t\tAssists\t\tPasses")
+        for i in playerList:
+            i.listPlayers()
+        print("")
+        """
+    if option =="2":
+    
+
+    if option =="3":
+
+    
+    if option =="4":
+
+    
+    if option =="5":
+
+    
+    if option =="6":
+    
+
+    """
+
+
+    if option =="7":
+        print("Exiting")  #if they want to exit, break
+        break
--- Player.py
+++ Player.py
@@ -1,5 +1,39 @@
 ####### Names and Spire ID ###########
-# 1. First Last ########
-# 2. First Last ########
+# 1. Ethan O'Connor 34445111
+# 2. Ryan Blanchette 34620003
 
 """ Implement the Player class and functions below """
+
+class Player:
+    def __init__(self,name,league,goals,assists,passes):
+        self.name = name
+        self.league = league
+        self.goals = goals
+        self.assists = assists
+        self.passes = passes
+    def listPlayers(self):
+        if len(self.name) <= 17 and len(self.name) > 10:
+            text = "{0:<0}\t\t{1:>6}\t{2:>13}{3:>18}\t{4:>25} 1".format(self.name,self.league,self.goals,self.assists,self.passes)
+        if len(self.name) == 10:
+            text = "{0:<0}{1:>20}\t{2:>13}{3:>18}\t{4:>18} 2".format(self.name,self.league,self.goals,self.assists,self.passes)
+        if len(self.name) > 17:
+            text = "{0:<0}{1:>12}\t{2:>13}{3:>18}\t{4:>18} 3".format(self.name,self.league,self.goals,self.assists,self.passes)
+        print(text)
+
+
+
+        #if len(self.name) <= 15 and len(self.league) < 7:
+        #                                                      # must check the length of the each string to allow for 
+        #    print(self.name,"",self.league,"",self.goals,self.assists,self.passes,"1",sep="\t")  # good looking formatting
+        #elif len(self.name) <= 15 and len(self.league) > 7:
+        #    print(self.name,"",self.league,self.goals,self.assists,self.passes,"2",sep="\t")        
+        #elif len(self.name) > 15 and len(self.league) >= 7:                                                   
+        #    print(self.name,self.league,"",self.goals,self.assists,self.passes,"3",sep="\t")
+        #elif len(self.name) <= 15 and len(self.league) == 7:
+        #    print(self.name,"",self.league,"",self.goals,self.assists,self.passes,"4",sep="\t")  
+
+        #else:
+        #    print(self.name,"\t",self.league,self.goals,self.assists,self.passes,sep="\t")
+    
+
+

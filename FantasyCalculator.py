48bf69fa65d896c68e3067daae322010b7caf811
etan8225 <eoconnor8225@gmail.com>
2024-04-02 23:38:10 -0400

    final

--- FantasyCalculator.py
+++ FantasyCalculator.py
@@ -3,19 +3,18 @@
 # 2. Ryan Blanchette 34260003
 
 """ Implement the main program and functions below """
-import Player
+import Player 
 
-
-P1 = Player.Player("Erling Haaland",     "EPL",          18, 5,  270)
-P2 = Player.Player("Robert Lewandowski", "La Liga",      12, 6,  414)
-P3 = Player.Player("Harry Kane",         "Bundesliga",   30, 6,  472)
-P4 = Player.Player("Son Heung-Min",      "EPL",          14, 8,  660)
-P5 = Player.Player("Julian Alvarez",     "EPL",          8,  8,  915)
-P6 = Player.Player("Leroy Sane",         "Bundesliga",   8, 11,  903)
-P7 = Player.Player("Mohamed Salah",      "EPL",          15, 9,  686)
-P8 = Player.Player("Jude Bellingham",    "La Liga",      16, 3, 1164)
-P9 = Player.Player("Toni Kroos",         "La Liga",      1,  7, 1908)
-P10= Player.Player("Sehrou Guirassy",   "Bundesliga",    21, 1,  494)
+P1 = Player.Player("Erling Haaland",     "EPL",          18,  5,  270)
+P2 = Player.Player("Robert Lewandowski", "La Liga",      12,  6,  414)
+P3 = Player.Player("Harry Kane",         "Bundesliga",   30,  6,  472)
+P4 = Player.Player("Son Heung-Min",      "EPL",          14,  8,  660)
+P5 = Player.Player("Julian Alvarez",     "EPL",           8,  8,  915)
+P6 = Player.Player("Leroy Sane",         "Bundesliga",    8, 11,  903)
+P7 = Player.Player("Mohamed Salah",      "EPL",          15,  9,  686)
+P8 = Player.Player("Jude Bellingham",    "La Liga",      16,  3, 1164)
+P9 = Player.Player("Toni Kroos",         "La Liga",       1,  7, 1908)
+P10= Player.Player("Sehrou Guirassy",    "Bundesliga",   21,  1,  494)
 
 playerList= [P1,P2,P3,P4,P5,P6,P7,P8,P9,P10]
 
@@ -34,24 +33,22 @@ print("====================\n\n")
 def display_goal(user_input):
     for i in range(10):
         player_being_checked=playerList[i]
-        if  player_being_checked.goals >= user_input:
+        if  player_being_checked.goals >= user_input:   # only print if player goals is greater than user input
             player_being_checked.listPlayers()
-            print("")
-        elif user_input>30:
-            print("no players found")
+        elif user_input>30:                             # no player has more than 30 goals, print if 
+            print("No players found.")                  # user input > 30
             break
             
 def display_league(user_input):
     count = 0
-    for i in range(10):
-        checking_league=playerList[i]
-        count = count + 1
+    for i in range(10):                         # displays players with specific league,
+        checking_league=playerList[i]           # and checks to see if none are printed to say
+        count = count + 1                       # no players found, if needed.
         if checking_league.league == user_input:
             count =  count - 1
             checking_league.listPlayers()
-            print("") 
         elif count == 10:
-            print("no players found")
+            print("No players found.")
             break
 
 def display_player(user_input):
@@ -59,56 +56,64 @@ def display_player(user_input):
     for i in range(10):
         checking_player=playerList[i]
         a=a+1
-        if checking_player.name == user_input:
-            checking_player.listPlayers()
-            print("")
-            a=a-1 
+        if checking_player.name == user_input:      # for loop to go through all the players
+            checking_player.listPlayers()           # and check if the name matches.
+            a=a-1                                   # also keeps track if no players were printed
         elif a==10:
-            print("no players found")
+            print("No players found.")
         
 def display_most_goals():
     best_players_goals=0
     for i in range(10):
-        player_being_checked=playerList[i]
-        if player_being_checked.goals > best_players_goals:
-            best_players_goals=player_being_checked.goals
-        else:
-            continue
+        player_being_checked=playerList[i]                      # for loop to go through players.
+        if player_being_checked.goals > best_players_goals:     # Set bestPlayerGoals to a higher value if that player
+            best_players_goals=player_being_checked.goals       # has more goals.
     for i in range (10):
-        player_being_checked2 = playerList[i]
-        if best_players_goals == player_being_checked2.goals:
-            print(f"{player_being_checked2.name} has the most goals with {player_being_checked2.goals}")
+        player_being_checked2 = playerList[i]                   # print out the player that matches that amount
+        if best_players_goals == player_being_checked2.goals:   # of goals
+            print(f"{player_being_checked2.name} has the most goals with {player_being_checked2.goals}.\n")
     
 def calculate_total_points():
     for i in range(10):
-        pl=playerList[i]
+        pl=playerList[i]                                      # goals = 2.5 pts per, assists is .93 per, passes are .05 per.
         total_points= float(pl.goals) * 2.5 + float(pl.assists)*.93 + float(pl.passes) * .05
         total_points_rounded=round(total_points,2)
-        print(f"{pl.name} has {total_points_rounded} points")
+        print(f"{pl.name} has {total_points_rounded} points.") # print out the player with x amount of fantasy points.
+    print("")       #provide a newline for the new menu prompt
         
 
 while True:
     display_menu()
     option=input("Enter Command: ")
-
     
     if option =="1":
         print("Name   \t\t\tLeague\t\tGoals\t\tAssists\t\tPasses")
-        for i in playerList:
+        for i in playerList:  # easy way to list all players
             i.listPlayers()
-        print("")
+        print("")       #provide a newline for the new menu prompt
         
     if option =="2":
-        min_num_of_goals=int(input("please indicate number of goals: "))
-        display_goal(min_num_of_goals)             
+        min_num_of_goals=int(input("Please indicate number of goals: "))
+        if min_num_of_goals <= 30:              # no players have more than 30 goals, dont print if variable > 30
+            print("Name   \t\t\tLeague\t\tGoals\t\tAssists\t\tPasses")
+        display_goal(min_num_of_goals)
+        print("")       #provide a newline for the new menu prompt      
 
     if option =="3":
-        selected_league=input("please indicate the league: ")
+        selected_league=input("Please indicate the league: ")
+        if selected_league == "EPL" or selected_league == "Bundesliga" or selected_league == "La Liga":
+            print("Name   \t\t\tLeague\t\tGoals\t\tAssists\t\tPasses")      # only print for valid leagues
         display_league(selected_league)
+        print("")      #provide a newline for the new menu prompt
    
     if option =="4":
-        selected_player=input("please indicate name of player: ")
+        selected_player=input("Please indicate name of player: ")
+        for i in range(10):
+            player = playerList[i]                          # check if name exists, if it does, print out name/league...
+            if player.name == selected_player:
+                print("Name   \t\t\tLeague\t\tGoals\t\tAssists\t\tPasses") # only print if the player name is in the list
         display_player(selected_player)
+        print("")      #provide a newline for the new menu prompt
     
     if option =="5":
         display_most_goals()
@@ -117,5 +122,5 @@ while True:
         calculate_total_points()
 
     if option =="7":
-        print("Exiting")  #if they want to exit, break
+        print("Exiting")  # if they want to exit, break
         break
--- Player.py
+++ Player.py
@@ -1,18 +1,18 @@
 ####### Names and Spire ID ###########
 # 1. Ethan O'Connor 34445111
-# 2. Ryan Blanchette 34620003
+# 2. Ryan Blanchette 34260003
 
 """ Implement the Player class and functions below """
 
 class Player:
     def __init__(self,name,league,goals,assists,passes):
         self.name = name
-        self.league = league
+        self.league = league            # all players have these attributes
         self.goals = goals
         self.assists = assists
         self.passes = passes
-    def listPlayers(self):
-        if len(self.name) <= 17 and len(self.name) > 10:
+    def listPlayers(self):                                  #this is so the names are printed nicely. I tried my best to not use "magic numbers"
+        if len(self.name) <= 17 and len(self.name) > 10:    #but i don't believe this is possible to do (easily and quickly) in one line.
             text = "{0:<0}\t\t{1:>6}\t{2:>13}{3:>18}\t{4:>14}".format(self.name,self.league,self.goals,self.assists,self.passes)
         if len(self.name) == 10:
             text = "{0:<0}{1:>20}\t{2:>13}{3:>18}\t{4:>14}".format(self.name,self.league,self.goals,self.assists,self.passes)
@@ -23,21 +23,5 @@ class Player:
         if len(self.name) == 15 and len(self.league) == 10:
             text = "{0:<0}\t{1:>14}\t{2:>13}{3:>18}\t{4:>14}".format(self.name,self.league,self.goals,self.assists,self.passes)
         print(text)
-
-
-
-        #if len(self.name) <= 15 and len(self.league) < 7:
-        #                                                      # must check the length of the each string to allow for 
-        #    print(self.name,"",self.league,"",self.goals,self.assists,self.passes,"1",sep="\t")  # good looking formatting
-        #elif len(self.name) <= 15 and len(self.league) > 7:
-        #    print(self.name,"",self.league,self.goals,self.assists,self.passes,"2",sep="\t")        
-        #elif len(self.name) > 15 and len(self.league) >= 7:                                                   
-        #    print(self.name,self.league,"",self.goals,self.assists,self.passes,"3",sep="\t")
-        #elif len(self.name) <= 15 and len(self.league) == 7:
-        #    print(self.name,"",self.league,"",self.goals,self.assists,self.passes,"4",sep="\t")  
-
-        #else:
-        #    print(self.name,"\t",self.league,self.goals,self.assists,self.passes,sep="\t")
-    
 
 
